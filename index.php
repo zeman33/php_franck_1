f5f54c2e33a5c7950993ba1d503d8ba1dd3dddd3
José <zezeman33@gmail.com>
2022-01-24 12:12:11 +0100

    jo

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+$maVariable = ["titi" , "tata", "toto"];
+
+$monBooleen =true;
+
+//echo $maVariable[0];
+//echo $maVariable[1];
+//echo $maVariable[2];
+
+/*$index = 0;  
+
+while($index <= 2){
+    echo $maVariable[$index];
+    $index = $index + 1;
+
+}*/
+
+//------
+
+/*for($index = 0; $index < count($maVariable) ; $index++){
+    echo $maVariable[$index];
+}*/
+
+foreach ($maVariable as $monNom){
+    echo $monNom;
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
